9fc2984cc88aa5ef53fc6bf25833cbc8dd0888e8
apps <23049337+yevgeni-shapiro-max2006@users.noreply.github.com>
2026-08-24 23:12:36 +0300

    Update main.tf

--- modules/splunk/main.tf
+++ modules/splunk/main.tf
@@ -1,1 +1,14 @@
+resource "helm_release" "splunk_operator" {
+  name             = "splunk-operator"
+  namespace        = "splunk-operator"
+  create_namespace = true
 
+  repository = "https://splunk.github.io/splunk-operator/"
+  chart      = "splunk-operator"
+
+  # Optional: pin a specific version
+  version    = "3.1.0"
+
+  wait    = true
+  timeout = 600
+}
