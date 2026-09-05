56564f0c018ae050c45514cd5ce31eca85f8c262
apps <23049337+yevgeni-shapiro-max2006@users.noreply.github.com>
2026-09-05 23:23:57 +0300

    Change S3 key for Terraform backend configuration

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "apps-terraform"
-    key    = "eks-logs-aggregation/terraform.tfstate"
+    key    = "eks-blockscout/terraform.tfstate"
     region = "eu-west-1"
   }
 }
