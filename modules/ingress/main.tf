6a8ca4a5ee95e8224b7a9175ea0631ba8d661eaa
apps <23049337+yevgeni-shapiro-max2006@users.noreply.github.com>
2026-09-06 00:07:53 +0300

    update
    Updated ingress configuration to use 'monitor.appflex.io' instead of 'logs.appflex.io' and removed the minio ingress resource.

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -16,12 +16,12 @@ resource "kubernetes_ingress_v1" "grafana" {
     ingress_class_name = "kong"
 
     tls {
-      hosts       = ["logs.appflex.io"]
+      hosts       = ["monitor.appflex.io"]
       secret_name = "loki-tls"
     }
 
     rule {
-      host = "logs.appflex.io"
+      host = "monitor.appflex.io"
       http {
         path {
           path      = "/"
@@ -40,44 +40,3 @@ resource "kubernetes_ingress_v1" "grafana" {
   }
 }
 
-
-resource "kubernetes_ingress_v1" "minio" {
-  metadata {
-    name      = "ingress-route-minio"
-    namespace = "default"
-    annotations = {
-      "konghq.com/strip-path" = "true"
-      # Optional:
-      # "konghq.com/protocols"                  = "https"
-      # "konghq.com/https-redirect-status-code" = "301"
-      # "cert-manager.io/cluster-issuer"        = "letsencrypt-prod"
-    }
-  }
-
-  spec {
-    ingress_class_name = "kong"
-
-    tls {
-      hosts       = ["logs-s3.appflex.io"]
-      secret_name = "loki-tls"
-    }
-
-    rule {
-      host = "logs-s3.appflex.io"
-      http {
-        path {
-          path      = "/"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = "minio-console"
-              port {
-                number = 9001
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
